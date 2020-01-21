c85754d01d0e81b3da9e815e4c5afde0b0a33f03
Jeremy Xie <climen712@gmail.com>
2020-01-21 12:03:32 +0000

    test commit

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -6,14 +6,14 @@ import java.util.ArrayList;
 public class QueryProcessor {
 
     public String process(String query) {
-//        if (query.toLowerCase().contains("shakespeare")) {
-//            return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
-//                    "English poet, playwright, and actor, widely regarded as the greatest " +
-//                    "writer in the English language and the world's pre-eminent dramatist.";
-//        }
-        if (query.toLowerCase().contains("86599200")){
-            return "20802";
+        if (query.toLowerCase().contains("shakespeare")) {
+            return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
+                    "English poet, playwright, and actor, widely regarded as the greatest " +
+                    "writer in the English language and the world's pre-eminent dramatist.";
         }
+//        if (query.toLowerCase().contains("86599200")){
+//            return "20802";
+//        }
         if (query.toLowerCase().contains("largest")){
             String new_query = query.toLowerCase();
             ArrayList<Integer> allMatches = new ArrayList<Integer>();
