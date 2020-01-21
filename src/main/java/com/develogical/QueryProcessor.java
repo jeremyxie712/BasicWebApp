ff3428ad28bd7682c0329030e12eeb9443da1c8c
Jeremy Xie <climen712@gmail.com>
2020-01-21 11:15:10 +0000

    test

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -3,7 +3,7 @@ package com.develogical;
 public class QueryProcessor {
 
     public String process(String query) {
-        if (query.toLowerCase().contains("John")) {
+        if (query.toLowerCase().contains("shakespeare")) {
             return "For testing purposes only " +
                     "" +
                     "";
